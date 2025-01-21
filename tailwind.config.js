280745088becc91325dad286a640e5026ed6fcc7
박현우 <gusdn3477@naver.com>
2025-01-22 00:36:12 +0900

    chore: init tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}", // 파일 확장자에 맞게 추가
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
