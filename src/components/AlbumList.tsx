9878cdb419924ca7c9cfbc708b6e3169dc6e71f5
박현우 <gusdn3477@naver.com>
2025-01-22 23:06:02 +0900

    refactor: useSuspense queryFn 안에서 zustand store set 하도록 구현 & 해당 로직 페이지 컴포넌트로 끌어올림

--- src/components/AlbumList.tsx
+++ src/components/AlbumList.tsx
@@ -1,6 +1,5 @@
 import { useCallback, useState } from 'react';
-import { useSuspenseQuery } from '@tanstack/react-query';
-import AlbumRepository from '../repository/AlbumRepository';
+import { useAlbumStore } from '../store';
 import { formatDate } from '../util/formatDate';
 import AlbumItem from './AlbumItem';
 import BottomSheet from './BottomSheet';
@@ -8,28 +7,10 @@ import SwitchButton from './SwitchButton';
 
 export default function AlbumList() {
   const [bottomSheetOpen, setBottomSheetOpen] = useState(false);
-  const { data } = useSuspenseQuery({
-    queryKey: ['albumlist'],
-    queryFn: AlbumRepository.getAlbumList,
-  });
+  const albumList = useAlbumStore((state) => state.albumList);
 
-  const albumData = data.album_list.map((album) => ({
-    id: album.id,
-    title: album.title, // 앨범 이름
-    releaseDate: album.released_at, // 발매일
-    artistName: album.artist.name, // 가수 이름 (그룹 이름)
-    artistProfile: album.artist.profile_image || null, // 가수 프로필 사진 (필드가 없으니 null 처리)
-    typeCount: album.published_album_list.length, // 타입 개수
-    albumCount: album.count, // 앨범 수량
-    nfcImageUrl: album.published_album_list[0]?.nfc_image_url || null, // NFC 이미지 URL
-    boxImageUrl: album.published_album_list[0]?.box_image_url || null, // 박스 이미지 URL
-  }));
-
-  const totalAlbumCount = data.album_list.reduce((sum, album) => sum + album.count, 0); // 전체 갯수
-  const totalTypeCount = data.album_list.reduce(
-    (sum, album) => sum + album.published_album_list.length,
-    0
-  );
+  const totalAlbumCount = albumList.reduce((sum, album) => sum + album.albumCount, 0); // 전체 갯수
+  const totalTypeCount = albumList.reduce((sum, album) => sum + album.typeCount, 0);
 
   const handleBottomSheetOpen = useCallback(() => {
     setBottomSheetOpen(true);
@@ -43,7 +24,7 @@ export default function AlbumList() {
     <>
       <div className="flex flex-row items-center justify-between">
         <p>
-          <span className="text-sm text-[#6C6C6C]">전체 {data.album_list.length}</span>
+          <span className="text-sm text-[#6C6C6C]">전체 {albumList.length}</span>
           <span className="ml-2 text-[13px] text-[#A5A5A5]">
             (타입 {totalTypeCount} 수량 {totalAlbumCount})
           </span>
@@ -54,7 +35,7 @@ export default function AlbumList() {
         </div>
       </div>
       <ul className="mt-2">
-        {albumData.map((album) => (
+        {albumList.map((album) => (
           <AlbumItem
             key={album.id}
             title={album.title}
--- src/pages/AlbumManagement.tsx
+++ src/pages/AlbumManagement.tsx
@@ -1,8 +1,33 @@
+import { useSuspenseQuery } from '@tanstack/react-query';
 import AlbumList from '../components/AlbumList';
 import CloseButton from '../components/CloseButton';
 import InnerLayout from '../components/InnerLayout';
+import AlbumRepository from '../repository/AlbumRepository';
+import { useAlbumStore } from '../store';
 
 export default function AlbumManagement() {
+  const setAlbumList = useAlbumStore((state) => state.setAlbumList);
+
+  useSuspenseQuery({
+    queryKey: ['albumlist'],
+    queryFn: async () => {
+      const data = await AlbumRepository.getAlbumList();
+      const transformedData = data.album_list.map((album) => ({
+        id: album.id,
+        title: album.title, // 앨범 이름
+        releaseDate: album.released_at, // 발매일
+        artistName: album.artist.name, // 가수 이름 (그룹 이름)
+        artistProfile: album.artist.profile_image || null, // 가수 프로필 사진 (필드가 없으니 null 처리)
+        typeCount: album.published_album_list.length, // 타입 개수
+        albumCount: album.count, // 앨범 수량
+        nfcImageUrl: album.published_album_list[0]?.nfc_image_url || null, // NFC 이미지 URL
+        boxImageUrl: album.published_album_list[0]?.box_image_url || null, // 박스 이미지 URL
+      }));
+      setAlbumList(transformedData);
+      return transformedData; // 캐시에 저장
+    },
+  });
+
   return (
     <InnerLayout
       headerLeft={<h1 className="m-0 text-sm">앨범관리</h1>}
