442bc5b24d1016ba4de66829a8658cff53974b70
박현우 <gusdn3477@naver.com>
2025-01-23 00:27:18 +0900

    feat: useAlbumStore persist 써서 로컬스토리지에 데이터 동기화

--- src/store/useAlbumStore.ts
+++ src/store/useAlbumStore.ts
@@ -1,4 +1,5 @@
 import { create } from 'zustand';
+import { persist, createJSONStorage } from 'zustand/middleware';
 import { ImageFile } from '../types/ImageFile';
 
 interface Album {
@@ -11,8 +12,8 @@ interface Album {
   albumCount: number;
   nfcImageUrl: string | null;
   boxImageUrl: string | null;
-  isDownloaded: boolean;
-  isUpdated: boolean;
+  isDownloaded?: boolean;
+  isUpdated?: boolean;
 }
 
 interface AlbumStore {
@@ -23,29 +24,37 @@ interface AlbumStore {
   removeAlbum: (id: number) => void;
 }
 
-export const useAlbumStore = create<AlbumStore>((set) => ({
-  albumList: [],
+export const useAlbumStore = create<AlbumStore>()(
+  persist(
+    (set) => ({
+      albumList: [],
 
-  // 앨범 리스트 설정
-  setAlbumList: (albums) => set({ albumList: albums }),
+      // 앨범 리스트 설정
+      setAlbumList: (albums) => set({ albumList: albums }),
 
-  // 앨범 추가
-  addAlbum: (album) =>
-    set((state) => ({
-      albumList: [...state.albumList, album],
-    })),
+      // 앨범 추가
+      addAlbum: (album) =>
+        set((state) => ({
+          albumList: [...state.albumList, album],
+        })),
 
-  // 앨범 업데이트
-  updateAlbum: (id, updatedData) =>
-    set((state) => ({
-      albumList: state.albumList.map((album) =>
-        album.id === id ? { ...album, ...updatedData } : album
-      ),
-    })),
+      // 앨범 업데이트
+      updateAlbum: (id, updatedData) =>
+        set((state) => ({
+          albumList: state.albumList.map((album) =>
+            album.id === id ? { ...album, ...updatedData } : album
+          ),
+        })),
 
-  // 앨범 삭제
-  removeAlbum: (id) =>
-    set((state) => ({
-      albumList: state.albumList.filter((album) => album.id !== id),
-    })),
-}));
+      // 앨범 삭제
+      removeAlbum: (id) =>
+        set((state) => ({
+          albumList: state.albumList.filter((album) => album.id !== id),
+        })),
+    }),
+    {
+      name: 'album-storage',
+      storage: createJSONStorage(() => localStorage),
+    }
+  )
+);
