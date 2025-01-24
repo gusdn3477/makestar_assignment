6234da31776574e7593710ce9ef78f72a086c712
박현우 <gusdn3477@naver.com>
2025-01-24 18:16:54 +0900

    chore: API Token .env 파일로 분리

--- src/util/API.ts
+++ src/util/API.ts
@@ -1,14 +1,11 @@
 import axios from 'axios';
 
 const BASE_URL = '/apis/v1/pocaalbum';
-const TOKEN =
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxODAzNzgxNjU1LCJpYXQiOjE3MTc0NjgwNTUsImp0aSI6IjM3NTlhOGFjMzg5YjRmM2I5YTQ1M2QwNjk4NzlkMTQzIiwidXNlcl9pZCI6MTczNTE2Niwib3JpZ2luX3VzZXJfaWQiOjE3MzUxNjYsInVzZXIiOiI9MDJiajVDYnBGV2JuQmtjaFIzY2x0V1l0NUNkcFZuY2pWbWMiLCJsb2dpbl9tZXRob2QiOiJlbWFpbCIsImxlZ2FjeV91c2VyX2lkIjotMX0.5ko1VkSMiLsCbZun4PxZaqYmHjdn0JC9Qk54BJ-EOpA';
-
 export const API = axios.create({
   baseURL: BASE_URL,
   headers: {
     'Content-Type': 'application/json',
-    Authorization: `Bearer ${TOKEN}`,
+    Authorization: `Bearer ${import.meta.env.VITE_API_TOKEN}`,
   },
 });
 
