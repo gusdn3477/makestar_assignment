34e1ea5de5456ee10beb98c76b84cafb071611ef
박현우 <gusdn3477@naver.com>
2025-01-24 18:08:34 +0900

    feat: proxy 설정 통하여 ip로도 접속 허용(cors 이슈 해결)

--- vite.config.ts
+++ vite.config.ts
@@ -10,4 +10,14 @@ export default defineConfig({
       plugins: [tailwindcss()],
     },
   },
+  server: {
+    proxy: {
+      '/apis': {
+        target: 'https://dev-api.pocaalbum.com', // 프록시 요청을 보낼 서버 주소
+        changeOrigin: true, // 호스트 헤더 변경
+        rewrite: (path) => path.replace(/^\/apis/, '/apis'), // 경로 재작성
+        secure: true, // https 인증서가 필요한 경우 true
+      },
+    },
+  },
 });
