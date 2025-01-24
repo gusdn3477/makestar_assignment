613dd7539b05fdd2e1615ae11bec8977c8b23fec
박현우 <gusdn3477@naver.com>
2025-01-24 15:43:11 +0900

    feat: 홈에서 다운로드 목록 없는 케이스 임시 구현

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,9 +2,12 @@ import { useNavigate } from 'react-router-dom';
 import Carousel from '../components/Carousel';
 import Chip from '../components/Chip';
 import InnerLayout from '../components/InnerLayout';
+import { useAlbumStore } from '../store';
 
 export default function Home() {
   const navigate = useNavigate();
+  const albumList = useAlbumStore((state) => state.albumList);
+  const downloadAlbumList = albumList.filter((album) => album.isDownloaded);
 
   const handleChipClick = () => {
     navigate('/management');
@@ -18,8 +21,12 @@ export default function Home() {
         </>
       }
     >
-      <main>
-        <Carousel />
+      <main className="flex h-full w-full items-center justify-center">
+        {downloadAlbumList.length === 0 ? (
+          <span>다운로드 목록이 비어 있습니다. </span>
+        ) : (
+          <Carousel />
+        )}
       </main>
     </InnerLayout>
   );
